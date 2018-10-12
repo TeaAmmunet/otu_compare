f9bf66a6af06f336331126e151b01500b403306a
TeaAmmunet <tea.ammunet@gmail.com>
2018-10-12 13:29:00 +0200

    added good OTUs output and separation of bad sequences

--- otu_comparison2.py
+++ otu_comparison2.py
@@ -36,9 +36,12 @@ def its1_parse():
 def refcomp(lst1, d1, key=None):
 	if key == None:
 		k=lst1[0]
+		key_check="no"
+		print k, key_check
 	else:
 		k=key
-	#print "k=", k
+		key_check="yes"
+		print k, key_check
 	if not lst1:
 		pass
 	else:
@@ -48,30 +51,52 @@ def refcomp(lst1, d1, key=None):
 			comp=list(d1[lst1[0]])
 			comp.insert(0,lst1[0])
 			diff=set(ref) - set(comp)
+			print "diff",diff
 			symdif=set(ref) ^ set(comp)
+			print "symdiff",symdif
 			if diff==symdif:
-				return refcomp(list(diff), d1,k)
+				if key_check == "no": #how to get refcomp to run with both key and non-key?
+					print "no"
+					print diff
+					return refcomp(list(diff), d1 )
+				else:
+					return refcomp(list(diff), d1,k)
 			else:
-				return k
+				if key_check=="no":
+					return lst1
+				else:
+					return k
 		#if key in d1 values
 		elif lst1[0] in sum(d1.values(),[]):
 			new_key=[ke for (ke, va) in d1.items() if lst1[0] in va][0]
+			print "new_key", new_key
 			ref=lst1
 			comp=list(d1[new_key])
 			comp.insert(0,new_key)
 			diff=set(ref) - set(comp)
+			print "diff",diff
 			symdif=set(ref) ^ set(comp)
+			print "symdiff",symdif
 			if diff==symdif:
-				return refcomp(list(diff), d1, k)
+				if key_check == "no": #how to get refcomp to run with both key and non-key?
+					print "no"
+					print diff
+					return refcomp(list(diff), d1 )
+				else:
+					return refcomp(list(diff), d1, k)
 			else:
-				return k
+				if key_check =="no":
+					return lst1
+				else:
+					return k
 		else:
-			out="value not in dict"
+			out="value not in dict: {lst1[0]}"
 			return out
 	return
 
 def comparison(its2, its1):
 	chimers=[]
+	passed=[]
 	for key,values in its2.iteritems():
 		#print "starting key", key
 		if key in its1:
@@ -83,6 +108,8 @@ def comparison(its2, its1):
 			out1=refcomp(helplst, its1, key)
 			if out1 != None:
 				chimers.append(out1)
+			else:
+				passed.append(key)
 		if key in sum(its1.values(),[]):
 			new_key=[ke for (ke, va) in its1.items() if key in va][0]
 			group=list(its1[new_key])
@@ -97,15 +124,34 @@ def comparison(its2, its1):
 			#if output empty, don't add
 			if out != None:
 				chimers.append(out)
-	return chimers
+			else:
+				passed.append(key)
+	return passed, chimers
+
+def faulty_seqs(its2,its1,chimers):
+	all_seqs=[]
+	for i in chimers:
+		print i
+		helplist=its2[i]
+		helplist.insert(0,i)
+		print "calling refcomp with:",helplist
+		seqs=refcomp(helplist,its1)
+		all_seqs.append(seqs)
+	return sum(all_seqs,[])
+
 
 ####MAIN
 def main():
 	chims=[]
 	its2=its2_parse()
 	its1=its1_parse()
-	chims=comparison(its2,its1)
+	#First run, getting good and chimeric OTUs
+	passed,chims=comparison(its2,its1)
+	print passed
 	print chims
+	#Second step: getting sequences split between OTUs
+	faulty=faulty_seqs(its2, its1,chims)
+	print faulty
 	return
 
 main()
